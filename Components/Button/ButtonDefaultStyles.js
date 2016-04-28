10a49cf84f1e9c68d42533b39a92275aedfa6484
ewei <ewei9046des@hotmail.com>
2016-04-28 16:46:41 +0800

    修改btn样式

--- Components/Button/ButtonDefaultStyles.js
+++ Components/Button/ButtonDefaultStyles.js
@@ -3,35 +3,68 @@
 import {StyleSheet} from 'toothless_scale';
 
 module.exports = StyleSheet.create({
-    buttonBox: {
-        height: 35,
+    //默认按钮
+    buttonBox_deafult: {
         alignItems: 'center',
         justifyContent: 'center',
+        height: 35,
+        borderRadius: 2,
+        backgroundColor: '#616B72',
         paddingLeft: 16,
         paddingRight: 16,
         margin: 4,
-        borderRadius: 2,
-        backgroundColor: '#0D7EFF',
     },
-    buttonText: {
+    buttonText_deafult: {
         fontSize: 14,
         color: '#fff',
         textAlign: 'center',
         fontFamily: 'PingFang SC',
     },
+    //默认按钮Disable
+    buttonBox_deafult_disabled: {
+        opacity: 0.1,
+    },
+    buttonText_deafult_disabled: {
+    },
+
+    //主要按钮
+    buttonBox_primary:{
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: 35,
+        borderRadius: 2,
+        backgroundColor: '#0D7EFF',
+        paddingLeft: 16,
+        paddingRight: 16,
+        margin: 4,
+    },
+    buttonText_primary:{
+    },
+    //主要按钮Disable
+    buttonBox_primary_disabled: {
+        opacity: 0.1,
+    },
+    buttonText_primary_disabled: {
+    },
 
     //幽灵按钮
-    buttonFlatBox: {
+    buttonBox_flat: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: 35,
+        borderRadius: 2,
         backgroundColor: 'rgba(0,0,0,0)',
-        paddingLeft: 8,
-        paddingRight: 8,
+        paddingLeft: 16,
+        paddingRight: 16,
+        margin: 4,
     },
-    buttonFlatText: {
+    buttonText_flat: {
         color: '#333',
     },
-
-    //不可点击
-    buttonDisableBox: {
-        opacity: 0.14,
-    }
+    //幽灵按钮Disable
+    buttonBox_flat_disabled: {
+        opacity: 0.1,
+    },
+    buttonText_flat_disabled: {
+    },
 });
